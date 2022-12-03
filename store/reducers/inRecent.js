27187e6284a799167be274b4abf7430eb3604661
oumarmaiga <oumarm611@gmail.com>
2022-12-03 15:55:09 +0000

    InRecent

--- store/reducers/inRecent.js
+++ store/reducers/inRecent.js
@@ -7,28 +7,32 @@ export default (state = initialState, action) => {
   let nextState;
   switch (type) {
     case "ADD_INRECENT":
-      console.log("ADD_INRECENT");
+      console.log("INRECENT_REDUCER");
       const inRecentFilmIndex = state.inRecentsFilm.findIndex(item => item.id === payload.id);
       if (inRecentFilmIndex !== -1)
       {
-        console.log("DELETE");
-        // On supprime le film dans l'historique
+        arr = state.inRecentsFilm;
+        const toIndex = 0;
+        // On recupere l'element a travers son index
+        const element = arr.splice(inRecentFilmIndex, 1)[0];
+        // On change la place de l'element
+        arr.splice(toIndex, 0, element);
+        // On applique la nouvelle array
         nextState = {
           ...state,
-          inRecentsFilm: state.inRecentsFilm.filter((item, index) => index !== inRecentFilmIndex),
-          //inRecentsFilm: [...state.inRecentsFilm, state.inRecentsFilm.unshift(state.inRecentsFilm.splice(state.inRecentsFilm.findIndex(item => item.id === inRecentFilmIndex), 1)[0])],
+          inRecentsFilm: arr,
           loading: false
         }
         
       }
-      console.log("ADD");
-      // On ajoute le film dans l'historique
-      nextState = {
-        ...state,
-        // inRecentsFilm: state.inRecentsFilm.unshift(payload),
-        inRecentsFilm: [...state.inRecentsFilm, payload],
-        //inRecentsFilm: [...state.inRecentsFilm, state.inRecentsFilm.unshift(payload)],
-        loading: false
+      else
+      {
+        // On ajoute le film dans l'historique
+        nextState = {
+          ...state,
+          inRecentsFilm: [payload, ...state.inRecentsFilm],
+          loading: false
+        }
       }
       
       return nextState || state;
